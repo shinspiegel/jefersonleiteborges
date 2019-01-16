5b7a17fbf45b495ff73493994fbe8b91bf73faa2
Jeferson 'Shin <shin.jef@gmail.com>
2019-01-16 18:38:49 -0200

    Starting point and json working.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React, { Component } from "react";
+import jeferson from "./json/jeferson.json";
 
 class App extends Component {
   render() {
+
+    //console log json file
+    console.log(jeferson);
+
     return (
-      <>
-        <div className="conteudo">teste</div>
-      </>
+      <div className="conteudo">
+        <div className="header">
+          <h1 className="header_heading">{jeferson.basicInfo.name}</h1>
+        </div>
+      </div>
     );
   }
 }
