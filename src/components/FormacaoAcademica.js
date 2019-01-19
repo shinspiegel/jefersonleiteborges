a784bbecd29ca777b0524967be3a3c07fc239908
Jeferson Shin <shin.jef@gmail.com>
2019-01-19 19:03:38 -0200

    Change the bold on the basic info

--- src/components/FormacaoAcademica.js
+++ src/components/FormacaoAcademica.js
@@ -1,17 +1,18 @@
 import React from 'react';
 
 const formacaoAcademica = (props) => {
-    
-    let listaCursos = props.academica.map(curso => (
+
+    const listaCursos = props.academica.map(curso => (
         <div key={curso.id} className="academica__caixaItem">
             <p className="academica__instituicao">{curso.instituicao}</p>
-            <p className="academica__curso">{curso.curso}</p>
+            <p className="academica__curso">Curso: <span className='negrito'>{curso.curso}</span></p>
             <p className="academica__anoConclusao">{curso.anoConclusao}</p>
         </div>
     ));
 
     return (
         <div className='academica'>
+            <h2 className='titulo--2'>Formação Academica</h2>
             {listaCursos}
         </div>
     );
--- src/components/InfoBasico.js
+++ src/components/InfoBasico.js
@@ -7,19 +7,20 @@ const infoBasico = (props) => {
     return (
         <section className='infoBasico'>
             <p className='paragrafo'>
-                <span className='negrito'>Nascimento: </span> <span className='toolTip'>{props.info.nascimento.d}/{props.info.nascimento.m}/{props.info.nascimento.a} <span className='toolTip__item'> Idade Atual {idadeAtual} </span></span>
+                Nascimento:&nbsp;
+                <span className='negrito toolTip'>{props.info.nascimento.d}/{props.info.nascimento.m}/{props.info.nascimento.a} <span className='toolTip__item'> Idade Atual {idadeAtual} </span></span>
             </p>
             <p className='paragrafo'>
-                <span className='negrito'>Sexo: </span>
-                {props.info.sexo}
+                Sexo:&nbsp;
+                <span className='negrito'>{props.info.sexo}</span>
             </p>
             <p className='paragrafo'>
-                <span className='negrito'>CPF: </span>
-                {props.info.cpf}
+                CPF:&nbsp;
+                <span className='negrito'>{props.info.cpf}</span>
             </p>
             <p className='paragrafo'>
-                <span className='negrito'>Estado Civil: </span>
-                {props.info.estadoCivil}
+                Estado Civil:&nbsp;
+                <span className='negrito'>{props.info.estadoCivil}</span>
             </p>
         </section>
     );
