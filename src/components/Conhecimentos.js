9b56aef4dec0121b6211632f79d51f09d0aa189b
Jeferson Shin <shin.jef@gmail.com>
2019-01-19 22:02:10 -0200

    Knowleged working. Need refinement.

--- src/components/Conhecimentos.js
+++ src/components/Conhecimentos.js
@@ -1,16 +1,32 @@
 import React from 'react';
+import estCheia from  '../img/icons.svg';
 
 const conhecimentos = (props) => {
-    
-    let listaConhecimento = props.conhecimentos.map( conhecimento => (
-        <div key={conhecimento.id}>
-            <p>{conhecimento.conhecimento} + {conhecimento.rank} out of 10</p>
+    function loopEstrelas(x, type) {
+        let estrelas = [] 
+        for (let i = 0; i < x; i++) {
+            estrelas.push(i);
+        }
+
+        let listaEstrela = estrelas.map((i) => (
+            <svg className='conhecimento__estrela'><use href={estCheia + '#' + type}></use></svg>
+        ));
+
+        return listaEstrela;
+    }
+
+    let listaConhecimento = props.conhecimentos.map(item => (
+        <div key={item.id} className='conhecimento'>
+            <p className='conhecimento__nome'>{item.conhecimento}</p>
+            {loopEstrelas( Math.floor(item.rank / 2) , 'starFull')}
+            {loopEstrelas( (item.rank % 2), 'starHalf')}
+            {loopEstrelas( Math.floor( (10 - item.rank) / 2) , 'starEmpty')}
         </div>
     ));
 
-
     return (
         <div className='conhecimentos'>
+            <h2 className='titulo--2'>Conhecimentos</h2>
             {listaConhecimento}
         </div>
     );
