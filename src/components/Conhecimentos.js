be943484269e7efdf319ba7c51644eaabeef994c
Jeferson Shin <shin.jef@gmail.com>
2019-01-19 22:11:10 -0200

    Some additional refinement. Need to improve the code.

--- src/components/Conhecimentos.js
+++ src/components/Conhecimentos.js
@@ -1,15 +1,17 @@
 import React from 'react';
-import estCheia from  '../img/icons.svg';
+import estCheia from '../img/icons.svg';
 
 const conhecimentos = (props) => {
     function loopEstrelas(x, type) {
-        let estrelas = [] 
+        let estrelas = []
         for (let i = 0; i < x; i++) {
-            estrelas.push(i);
+            estrelas.push(i + type);
         }
 
         let listaEstrela = estrelas.map((i) => (
-            <svg className='conhecimento__estrela'><use href={estCheia + '#' + type}></use></svg>
+            <svg key={i} className={'conhecimento__estrela conhecimento__estrela--' + type}>
+                <use href={estCheia + '#' + type}></use>
+            </svg>
         ));
 
         return listaEstrela;
@@ -18,9 +20,9 @@ const conhecimentos = (props) => {
     let listaConhecimento = props.conhecimentos.map(item => (
         <div key={item.id} className='conhecimento'>
             <p className='conhecimento__nome'>{item.conhecimento}</p>
-            {loopEstrelas( Math.floor(item.rank / 2) , 'starFull')}
-            {loopEstrelas( (item.rank % 2), 'starHalf')}
-            {loopEstrelas( Math.floor( (10 - item.rank) / 2) , 'starEmpty')}
+            {loopEstrelas(Math.floor(item.rank / 2), 'starFull')}
+            {loopEstrelas((item.rank % 2), 'starHalf')}
+            {loopEstrelas(Math.floor((10 - item.rank) / 2), 'starEmpty')}
         </div>
     ));
 
