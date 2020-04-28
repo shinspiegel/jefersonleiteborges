f09c5bd22f0d384aea6df026976e8ed6e5c6b8b0
Shin <shin.jef@gmail.com>
2020-04-28 19:46:52 -0300

    Under Construction

--- src/App.js
+++ src/App.js
@@ -1,53 +1,16 @@
 import React, { Component } from 'react';
+import './App.css';
+import './Main.css';
+const Home = () => (
+  <>
+    <h1>
+      Hi, I'm
+      <strong> Jef</strong>, this is a under-construction site.
+      <br />
+      <br />
+      <span>🚧</span>
+    </h1>
+  </>
+);
 
-import jefersonPT from './json/jeferson.json';
-
-import Nome from './components/Nome';
-import Foto from './components/Foto';
-import InfoBasico from './components/InfoBasico';
-import Formacao from './components/FormacaoAcademica';
-import Conhecimentos from './components/Conhecimentos';
-import Contato from './components/Contato';
-import Portfolio from './components/Portfolio';
-import Experiencia from './components/Experiencia';
-import ChangeLanguage from './components/ChangeLanguage.js';
-
-class App extends Component {
-  state = {
-    language: 'EN',
-    info: { ...jefersonPT },
-  };
-
-  handleLanguage() {
-    if (this.state.language === 'BR') {
-      this.setState({ language: 'EN' });
-      //this.setState({ info: { ...jefersonEN } });
-    }
-    if (this.state.language === 'EN') {
-      this.setState({ language: 'BR' });
-      //this.setState({ info: { ...jefersonPT } });
-    }
-  }
-
-  render() {
-    return (
-      <div className="conteudo">
-        <ChangeLanguage changeLang={() => this.handleLanguage()} lang={this.state.language} />
-        <Nome nome={this.state.info.basicInfo.name} />
-        <Foto foto={this.state.info.basicInfo.foto} />
-        <InfoBasico lang={this.state.language} info={this.state.info.basicInfo} />
-        <Formacao lang={this.state.language} academica={this.state.info.formacao} />
-        <Conhecimentos lang={this.state.language} conhecimentos={this.state.info.conhecimentos} />
-        <Contato
-          lang={this.state.language}
-          contato={this.state.info.otherInfo.contato}
-          endereco={this.state.info.otherInfo.endereco}
-        />
-        <Experiencia lang={this.state.language} experiencia={this.state.info.experiencia} />
-        <Portfolio lang={this.state.language} portfolio={this.state.info.otherInfo.portfolio} />
-      </div>
-    );
-  }
-}
-
-export default App;
+export default Home;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './scss/main.scss';
-import App from './App';
+import Home from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-
+ReactDOM.render(<Home />, document.getElementById('root'));
