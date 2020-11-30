6268b8570c6520e9b62aca821151de0edc2db7f4
Shin <shin.jef@gmail.com>
2020-11-30 20:28:23 -0300

    feat: update test for better testing in the app

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,10 @@
 module.exports = {
-  verbose: true,
+  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
+  setupFilesAfterEnv: ['<rootDir>/setupTests.js'],
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/babel-jest',
+  },
   moduleNameMapper: {
-    '^.+\\.(css|less)$': '<rootDir>/cssStub.js',
+    '\\.(css|less|scss|sass)$': '<rootDir>/cssStub.js',
   },
 };
